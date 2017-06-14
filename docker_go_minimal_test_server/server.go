f2bf2dd478048b232d8192d8c874334b45704828
Axel Bock <mr.axel.bock@gmail.com>
2017-06-14 15:57:04 +0200

    update the go mimimal test server

---
+++ docker_go_minimal_test_server/server.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+)
+
+var my_color string = "a multi-colored shine on all surfaces"
+
+func handler_health(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "OK")
+}
+
+func handler_root(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, my_color)
+}
+
+func main() {
+	if len(os.Args) > 1 && os.Args[1] != "default" {
+		my_color = strings.ToUpper(os.Args[1])
+	}
+	http.HandleFunc("/", handler_root)
+	http.HandleFunc("/health", handler_health)
+	http.ListenAndServe(":80", nil)
+}
