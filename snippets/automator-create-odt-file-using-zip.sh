0a4c3030ab6289a89568d006807279321be53e9b
Axel Bock <ab@a3b3.de>
2023-10-14 10:59:39 +0200

    feat(snippets): add automator-create-odt-file-using-zip.sh

---
+++ snippets/automator-create-odt-file-using-zip.sh
@@ -0,0 +1,29 @@
+# shell: /bin/bash
+# params: as stdin
+
+SAVE_DIR="$PWD"
+
+while read input_dir ; do
+    [[ -d "$input_dir" ]] || continue
+
+    cd "$input_dir"
+
+    # https://stackoverflow.com/a/315113
+    touch _odt_create_log.txt
+    exec > _odt_create_log.txt
+    exec 2>&1
+
+    set -x
+    ODT_FILE="../$(basename "$input_dir").odt"
+    rm -f "$ODT_FILE"
+
+    # create ODT documents with ZIP: https://is.gd/TZmNOQ
+    # SOME NOTES:
+    #    - YES, the parameter order in 2nd command MATTERS!!
+    #    - NO EXTRA FILES (e.g. the log file) in the .odt document
+    zip      "$ODT_FILE" -0 mimetype
+    zip -ruo "$ODT_FILE"    .         -x mimetype _odt_create_log.txt
+
+    cd "$SAVE_DIR"
+    set +x
+done
