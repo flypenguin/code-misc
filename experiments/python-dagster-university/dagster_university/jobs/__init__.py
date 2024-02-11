b01f1412232307323e017c2b2626b709f361c64a
Axel Bock <ab@a3b3.de>
2024-02-11 19:40:50 +0100

    feat(experiments/dagster_university): lesson 7

---
+++ experiments/python-dagster-university/dagster_university/jobs/__init__.py
@@ -0,0 +1,14 @@
+from dagster import AssetSelection, define_asset_job
+from ..partitions import monthly_partition, weekly_partition
+
+trips_by_week = AssetSelection.keys("trips_by_week")
+
+trip_update_job = define_asset_job(
+    name="trip_update_job",
+    selection=AssetSelection.all() - trips_by_week,
+)
+
+weekly_update_job = define_asset_job(
+    name="weekly_update_job",
+    selection=trips_by_week,
+)
